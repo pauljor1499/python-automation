db0906a02462434a0e125d1de54e91bd7da3a16d
Paul J <jorpaulvincent@gmail.com>
2022-03-29 10:08:40 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<router-view />
+</template>
+
+<style>
+	@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap");
+	#app {
+		font-family: "Poppins", sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+
+	a {
+		text-decoration: none;
+	}
+</style>
