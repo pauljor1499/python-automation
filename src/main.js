0cc840df8d9546c059332be80fc9525656c36715
Paul J <jorpaulvincent@gmail.com>
2022-04-04 10:13:05 +0800

    create test automation

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import naive from "naive-ui";
-
-const app = createApp(App);
-app.use(naive);
-
-app.use(router).mount("#app");
